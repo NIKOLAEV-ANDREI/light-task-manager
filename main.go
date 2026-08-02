5c7b9423c2f4a26742da565fb464a1e481e99f9f
ItsNotUber <114165477+NIKOLAEV-ANDREI@users.noreply.github.com>
2026-08-02 15:41:41 +0300

    Реализация просмотра задач

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type Task struct {
 
 func showMenu() {
 
-	fmt.Println("Менеджер задач\n")
+	fmt.Println("[Менеджер задач]\n")
 	fmt.Println("1. Добавить задачу")
 	fmt.Println("2. Просмотреть задачи")
 	fmt.Println("3. Отметить задачу как выполненную")
@@ -24,13 +24,28 @@ func showMenu() {
 func addTask(tasks *[]Task) {
 	var newTask Task
 
-	fmt.Println("Введите название задачи:")
+	fmt.Println("[Введите название задачи:]")
 	fmt.Scan(&newTask.Title)
-	fmt.Println("Введите описание задачи:")
+	fmt.Println("[Введите описание задачи:]")
 	fmt.Scan(&newTask.Description)
 
 	*tasks = append(*tasks, newTask)
-	fmt.Println("Задача добавлена!\n")
+	fmt.Println("[Задача добавлена!]\n")
+}
+
+func showTasks(tasks []Task) {
+	if len(tasks) == 0 {
+		fmt.Println("[Нет задач для отображения!]\n")
+	} else {
+		fmt.Println("[Список задач]:")
+		for i, task := range tasks {
+			status := "Не выполнена"
+			if task.Done {
+				status = "Выполнена"
+			}
+			fmt.Printf("%d. %s - %s [%s]\n", i+1, task.Title, task.Description, status)
+		}
+	}
 }
 
 func main() {
@@ -39,23 +54,23 @@ func main() {
 
 	for {
 		showMenu()
-		fmt.Println("Выберите действие:")
+		fmt.Println("[Выберите действие:]")
 		fmt.Scan(&choice)
 
 		switch choice {
 		case "0":
-			fmt.Println("Выход из менеджера задач")
+			fmt.Println("[Выход из менеджера задач]")
 			return
 		case "1":
 			addTask(&tasks)
 		case "2":
-			fmt.Println("ФУНКЦИЯ-2")
+			showTasks(tasks)
 		case "3":
-			fmt.Println("ФУНКЦИЯ-3")
+			fmt.Println("[ФУНКЦИЯ-3]")
 		case "4":
-			fmt.Println("ФУНКЦИЯ-4")
+			fmt.Println("[ФУНКЦИЯ-4]")
 		default:
-			fmt.Println("Неизвестная команда. Пожалуйста, выберите действие из меню.")
+			fmt.Println("[Неизвестная команда. Пожалуйста, выберите действие из меню.]\n")
 		}
 	}
 }
